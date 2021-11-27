43b97763d7d529fae8354a8946d9ce7937ef5ad4
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-11-27 09:04:08 +0200

    Added the broken .png to the alt of the "about" component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -63,7 +63,7 @@ export default function About(){
                     <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 padding">
                         <div class="text-box">
                             <div class="box">
-                                <i><img src="images/5.png"/></i>
+                                <i><img src="images/5.png" alt="../../public/images/brokenImage.png"/></i>
                                 <h3>MEET OUR Music STARS</h3>
                                 <p>It is a long established fact that a reader will be distracted by the readable
                                     <br/> content of a page when looking at its layout. The point of using Lorem
