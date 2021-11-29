92c93e8ad2e134b03534409072c1efde7e9309d1
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-11-29 10:01:11 +0200

    Added a test Login page just to test the link routing. It works...for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom';
 
 import Header from './components/Header'
 import Home from './components/Home'
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
        
 
       </Routes>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ export default function Header(){
                 <Navbar.Brand href="#home">AlbumRepo</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="#pricing">Register</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
