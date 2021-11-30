c5238ca594c5a551d21985de177a9bef9a105ae3
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-11-30 08:11:06 +0200

    Added the services component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import HomeBanner from '../HomeBanner';
+import OurServices from '../OurServices';
 
 export default function Home(){
 
     return(
 
+    <>
         <HomeBanner />
+        <OurServices />
+        
+    </>
 
     )
 
