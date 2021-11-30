79b21e973d718370c87db89fc09a53fc2869fda2
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-11-30 18:11:44 +0200

    Fixed the CSS in the login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,53 @@
+import './Login.css'
+
 export default function Login(){
 
     return(
 
-        <h1>Login Page</h1>
+        <>
+
+            <div className="contactbg">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="contacttitlepage">
+                                <h2>login</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className=" col-md-6 offset-md-3">
+                        <div className="address">
+
+                            <form>
+                                <div className="row">
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Email" type="text" name="Name" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Password" type="text" name="Email" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Email" type="text" name="Email" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <textarea className="textarea" placeholder="Message" type="text" name="Message"></textarea>
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <button className="send">Send</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
 
     )
 
