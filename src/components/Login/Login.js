2b5383f2e3d8051285557b0c2466a8316c55b679
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-01 22:14:20 +0200

    added some CSS stylings to the register link

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,21 @@
 import { Link } from 'react-router-dom'
-import './Login.css'
+import styles from './Login.module.css'
 
 export default function Login(){
+
+    const onLogin = (e) =>{
+
+        e.preventDefault()
+
+        let formData = new FormData(e.currentTarget)
+
+        let email = formData.get('email')
+        let password = formData.get('password')
+        console.log(email)
+        console.log(password)
+
+
+    }
 
   return(
 
@@ -10,22 +24,30 @@ export default function Login(){
               <h5 className="offcanvas-title" id="offcanvasExampleLabel">Enter your details</h5>
               <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
-          <div className="offcanvas-body">
-              <div>
-                    <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                        <label htmlFor="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                        <label htmlFor="floatingPassword">Password</label>
-                    </div>
-              </div>
-              <div className="dropdown mt-3">
-              <button type="button" className="btn btn-success">Login</button>
-              </div>
-              <Link to="/register">Don't have an account?<br/>Click here to Register</Link>
-          </div>
+
+
+          <form id="loginForm" onSubmit={onLogin}>
+
+            <div className="offcanvas-body">
+                <div>
+                        <div className="form-floating mb-3">
+                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name="email" />
+                            <label htmlFor="floatingInput">Email address</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"name="password" />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                </div>
+                <div className="dropdown mt-3">
+                    <input type="submit" className="btn btn-success" value="Login" />
+                </div>
+            </div>
+
+          </form>
+
+            <Link className={styles.registerLink} to="/register">Don't have an account?<br/>Click here to Register</Link>
+
       </div>
 
   )
