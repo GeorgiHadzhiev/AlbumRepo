93c378c181aa8434c7823ec434acf65dbe48d92b
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-01 19:37:35 +0200

    Fixed up the register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,48 +1,57 @@
+import './Register.css'
+
 export default function Register(){
 
     return(
 
-        <>
+        <div className="contact-page">
+            <div className="contactbg">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="contacttitlepage">
+                                <h2>Register</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-        <div className="contactbg">
+            </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
-                        <div className="contacttitlepage">
-                            <h2>register</h2>
+                    <div className=" col-md-6 offset-md-3">
+                        <div className="address">
+
+                            <form>
+                                <div className="row">
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="First Name" type="text" name="Name" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Last Name" type="text" name="Name" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Email" type="text" name="Email" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Password" type="password" name="Email" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <input className="contactus" placeholder="Repeat Password" type="password" name="Email" />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <button className="send">Create Account</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
+
+     
+
         </div>
-
-        <div className="container">
-            <div className="row">
-                <div className=" col-md-6 offset-md-3">
-                    <div className="address">
-
-                        <form>
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <input className="contactus" placeholder="Email" type="text" name="Name" />
-                                </div>
-                                <div className="col-sm-12">
-                                    <input className="contactus" placeholder="Password" type="text" name="Email" />
-                                </div>
-                                <div className="col-sm-12">
-                                    <input className="contactus" placeholder="Repeat Password" type="text" name="Repeat Password" />
-                                </div>
-                                <div className="col-sm-12">
-                                    <button className="send">Send</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </>
 
     )
 
