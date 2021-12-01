73a8ae44fb55b18b172e081c414a91a35fef32db
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-01 16:35:18 +0200

    Added text to the login offCanvas and a link to the register page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Login.css'
 
 export default function Login(){
@@ -6,23 +7,24 @@ export default function Login(){
 
       <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
           <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+              <h5 className="offcanvas-title" id="offcanvasExampleLabel">Enter your details</h5>
               <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
               <div>
-              Login
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                        <label for="floatingPassword">Password</label>
+                    </div>
               </div>
               <div className="dropdown mt-3">
-              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-                  Dropdown button
-              </button>
-              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+              <button type="button" class="btn btn-success">Login</button>
               </div>
+              <Link to="/register">Don't have an account?<br/>Click here to Register</Link>
           </div>
       </div>
 
