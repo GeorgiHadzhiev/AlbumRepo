12105a74b57827b0bba76035758bbd10b3b1bcb8
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-03 23:29:08 +0200

    Was able to add and stylize a dropdown menu.
    Still needs more work but it's getting there

---
+++ src/components/Header/DropDown/DropDown.js
@@ -0,0 +1,11 @@
+import './DropDown.css'
+
+export default function DropDown(){
+
+    return(
+
+        <a href="/hello-world" className="dropDown">Hello world!</a>
+
+    )
+
+}
--- src/components/Header/UserNav/UserNav.js
+++ src/components/Header/UserNav/UserNav.js
@@ -1,13 +1,19 @@
+import { useState } from "react"
 
+import DropDown from "../DropDown";
 
 
 export default function UserNav(){
+
+    const [open,setOpen] = useState(false);
 
     return(
 
         <div id="userNav">
 
-            <img src="icons/avatar5.png" alt="Avatar" className="avatar" />
+            <img src="icons/avatar5.png" alt="Avatar" className="avatar" onClick={() => setOpen(!open)} />
+
+            {open && <DropDown />}
 
         </div>
 
