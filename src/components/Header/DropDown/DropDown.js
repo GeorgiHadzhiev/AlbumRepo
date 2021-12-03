d1bb1140df8942bc1ca995154c39805f0020ffb9
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-03 23:59:51 +0200

    was able to add the root class for the global CSS in the correct place

--- src/components/Header/DropDown/DropDown.js
+++ src/components/Header/DropDown/DropDown.js
@@ -1,10 +1,33 @@
-import './DropDown.css'
+import styles from './DropDown.module.css'
 
-export default function DropDown(){
+function DropDownItem(props){
 
     return(
 
-        <a href="/hello-world" className="dropDown">Hello world!</a>
+        <a href="/add" className={styles.dropDownItem}>
+
+            {props.children}
+
+        </a>
+
+    )
+
+
+
+}
+
+export default function DropDown(){
+
+    return( 
+
+        <div className={styles.dropDown}>
+            <DropDownItem >Add Page</DropDownItem>
+            <DropDownItem >Add Page</DropDownItem>
+
+        </div>
+
+
+
 
     )
 
