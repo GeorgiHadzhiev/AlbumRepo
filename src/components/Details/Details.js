9f67144f940ed8a46b075f0c6b192b0e43774f9b
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-04 03:23:25 +0200

    Added the basic marup for the details component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,7 +2,25 @@
 
 export default function Details(){
 
+    return(
 
+        <section id="details-page" className="details">
+            <div className="album-information">
+                <h3>Name: {album.name}</h3>
+                <p className="type">Year: {album.year}</p>
+                <p className="img"><img src={album.imageUrl} /></p>
+                <div className="actions">
+                    <a className="button" href="/edit">Edit</a>
+                    <a className="button" href="/delete">Delete</a>
+                    
+                </div>
+            </div>
+            <div className="album-description">
+                <h3>Description:</h3>
+                <p>{album.description}</p>
+            </div>
+        </section>
 
+    )
 
 }
