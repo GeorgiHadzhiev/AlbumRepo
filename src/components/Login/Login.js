a0d4a019786d6beca72b1b0d35fa7ce1d9abc264
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-04 05:19:15 +0200

    added the ability to scroll while the login OffCanvas is turned on

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -48,7 +48,7 @@ export default function Login(){
 
   return(
 
-      <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" >
+      <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" >
           <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasExampleLabel">Enter your details</h5>
               <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
