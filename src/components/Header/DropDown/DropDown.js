36ab9e00bac3bc7076e6dd7044d042664238dad2
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-04 04:49:28 +0200

    Fixed the DropDown profile menu to show links to the relvenat pages
    Before, it was just navigating using the useNavigate hook so it didn't actually show where you were going. It was a hack way to make it work. Now, it does show it, but it looks uglier. I guess I gotta compramise

--- src/components/Header/DropDown/DropDown.js
+++ src/components/Header/DropDown/DropDown.js
@@ -1,45 +1,8 @@
 import { useNavigate } from 'react-router'
 import styles from './DropDown.module.css'
+import { Link } from 'react-router-dom'
 
 
-function DropDownItem(props){
-
-    let navigate = useNavigate();
-
-    function onClickProfileHandler(props){
-        
-        
-        let page = props.target.textContent
-    
-        if(page === "My Profile"){
-    
-            navigate('/myProfile')
-    
-        }
-        else if(page === "Add an Album"){
-    
-            navigate('/add')
-    
-        }
-        else if(page === "Logout"){
-
-            navigate('/logout')
-
-        }
-    
-    }
-
-    return(
-
-        <div onClick={onClickProfileHandler} className={styles.dropDownItem}>
-
-            {props.children}
-
-        </div>
-
-    )
-
-}
 
 export default function DropDown(){
 
@@ -47,9 +10,9 @@ export default function DropDown(){
 
         <div className={styles.dropDown}>
             
-            <DropDownItem>My Profile</DropDownItem>
-            <DropDownItem>Add an Album</DropDownItem>
-            <DropDownItem>Logout</DropDownItem>
+            <Link to="/myProfile" className={styles.dropDownItemProfile}>My Profile</Link>
+            <Link to="/add" className={styles.dropDownItemAdd}>Add an Album</Link>
+            <Link to="/logout" className={styles.dropDownItemLogout}>Logout</Link>
 
         </div>
 
