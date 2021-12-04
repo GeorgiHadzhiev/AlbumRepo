0715ac0757428788305eff88b508e3f61e68e773
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-04 02:35:18 +0200

    Added an album card component

---
+++ src/components/Catalog/AlbumCard/AlbumCard.js
@@ -0,0 +1,19 @@
+export default function AlbumCard(){
+
+    return(
+
+        <li>
+            <h3>Name: </h3>
+            <p>Year: </p>
+            <p>
+                <img />
+            </p>
+            <a href="/details">Details</a>
+
+
+
+        </li>
+
+    )
+
+}
--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,11 +1,11 @@
-
+import AlbumCard from "./AlbumCard"
 
 export default function Catalog(){
 
     return(
 
 
-        <h2>Hey look, it's the catalog!</h2>
+        
 
     )
 
