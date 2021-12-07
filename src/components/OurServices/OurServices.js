b003abaab9bbfa28f68a880447b2c06bfece07d5
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-07 16:44:36 +0200

    Changed the text on the ourServices component

--- src/components/OurServices/OurServices.js
+++ src/components/OurServices/OurServices.js
@@ -10,22 +10,22 @@ export default function OurServices(){
                     <div className={styles.serviceBox}>
                         <div className={styles.forBox}>
                             <i><img src="icons/icon1.png" alt="icon" className={styles.servicePic}/></i>
-                            <h3 className={styles.serviceTitle}>Custom Player</h3>
-                            <p className={styles.serviceText}>t is a long established fact that a reader will be distracted by the readable </p>
+                            <h3 className={styles.serviceTitle}>Upload Albums</h3>
+                            <p className={styles.serviceText}>Sign up and start filling the Repository with info on classic records</p>
                         </div>
                     </div>
                     <div className={styles.serviceBox}>
                         <div className={styles.forBox}>
-                            <i><img src="icons/icon1.png" alt="icon" className={styles.servicePic}/></i>
-                            <h3 className={styles.serviceTitle}>Custom Player</h3>
-                            <p className={styles.serviceText}>t is a long established fact that a reader will be distracted by the readable </p>
+                            <i><img src="icons/icon4.png" alt="icon" className={styles.servicePic}/></i>
+                            <h3 className={styles.serviceTitle}>Detailed Information</h3>
+                            <p className={styles.serviceText}>Full info on the given album, it's tracklist and the musicians</p>
                         </div>
                     </div>
                     <div className={styles.serviceBox}>
                         <div className={styles.forBox}>
-                            <i><img src="icons/icon1.png" alt="icon" className={styles.servicePic}/></i>
-                            <h3 className={styles.serviceTitle}>Custom Player</h3>
-                            <p className={styles.serviceText}>t is a long established fact that a reader will be distracted by the readable </p>
+                            <i><img src="icons/icon3.png" alt="icon" className={styles.servicePic}/></i>
+                            <h3 className={styles.serviceTitle}>Ratings And Opinions</h3>
+                            <p className={styles.serviceText}>Leave comments, likes and disslikes. Share your opinion with the rest of the community</p>
                         </div>
                     </div>
 
