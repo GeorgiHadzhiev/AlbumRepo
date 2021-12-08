b847667eaa7afa346363ced114aa94dfa3b842b5
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-08 17:00:56 +0200

    Added the extra details in the Album card for the details

--- src/components/Catalog/AlbumCard/AlbumCard.js
+++ src/components/Catalog/AlbumCard/AlbumCard.js
@@ -6,8 +6,7 @@ export default function AlbumCard({album}){
 
         <li>
             <h3>Name: {album.name}</h3>
-            <p>Year: {album.year}</p>
-            <p>Description: {album.description}</p>
+            <p>Year: {album.date}</p>
             <p>
                 <img src={album.imageURL} alt="broken"/>
             </p>
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -75,8 +75,25 @@ export default function Details(){
             <section id="details-page" className="details">
                 <div className="album-information">
                     <h3>Name: {album.name}</h3>
-                    <p className="type">Year: {album.year}</p>
+                    <h4>Artist: {album.composerArtist}</h4>
+                    <p className="year">Year: {album.date}</p>
                     <p className="img"><img src={album.imageURL} alt="broken" /></p>
+                    <p className="numberOfSongs">Number of tracks: {album.numberOfSongs}</p>
+                    <p className="genre">Genre: {album.genre}</p>
+                    <div className="album-description">
+                        <h3>Description:</h3>
+                        <p>{album.description}</p>
+                    </div>
+                    <div className="tracklist">
+                        <h3>Tracklist:</h3>
+                        <p>{album.tracklist}</p>
+                    </div>
+                    <div className="personnel">
+                        <h3>Personnel:</h3>
+                        <p>{album.personnel}</p>
+                    </div>
+
+
                     <div className="actions">
 
                         {user._id && (user._id === album._ownerId
@@ -99,10 +116,6 @@ export default function Details(){
 
                         
                     </div>
-                </div>
-                <div className="album-description">
-                    <h3>Description:</h3>
-                    <p>{album.description}</p>
                 </div>
             </section>
 
