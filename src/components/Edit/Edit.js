5b7e4361b96736f44d5ce68b546004afca471878
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-10 06:39:11 +0200

    Manged to fully implement validation on the edit page combined with a scroll action to the wrong filed

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,4 +1,4 @@
-import { useState,useContext } from 'react';
+import { useState,useContext,useEffect } from 'react';
 import { useParams,useNavigate } from 'react-router-dom';
 
 import styles from './Edit.module.css'
@@ -6,29 +6,43 @@ import albumService from '../../services/albumService.js'
 import useAlbumState from '../../hooks/useAlbumState.js';
 import { AuthContext } from '../../contexts/AuthContext.js';
 
+
 export default function Edit(){
 
     const {albumId} = useParams();
     const {user} = useContext(AuthContext);
-    const [errors,setErrors] = useState({name: null});
+    const [formErrors,setFormErrors] = useState({});
     const [album] = useAlbumState(albumId);
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
 
     function albumEditSubmitHandler(e){
 
         e.preventDefault();
 
+        if(Object.values(formErrors).some(x => x !== null)){
+
+            let invalidFiled = Object.entries(formErrors).filter(x => x[1] !== null)[0]
+            let invalidEl = document.getElementsByName(invalidFiled[0])[0]
+
+            return invalidEl.scrollIntoView(invalidEl)
+
+        }
+    
+
+
         let formData = new FormData(e.currentTarget);
 
         let name = formData.get('name')
-        let composerArtist = formData.get('composer-artist')
+        let composerArtist = formData.get('artist')
         let date = formData.get('date')
-        let numberOfSongs = formData.get('number-of-songs')
+        let numberOfSongs = formData.get('tracks')
         let genre = formData.get('genre')
         let tracklist = formData.get('tracklist')
         let description = formData.get('description')
         let personnel = formData.get('personnel')
-        let imageURL = formData.get('imageURL')
+        let imageURL = formData.get('picture')
+
 
         albumService.update(albumId,{
 
@@ -44,32 +58,36 @@ export default function Edit(){
 
 
         },user.accessToken)
-        .then(res =>{
+        .then(() =>{
 
             navigate(`/details/${albumId}`)
 
         })
         
-
-
     }
 
-    function nameChangeHandler(e){
 
-        let currentName = e.target.value
 
-        if(currentName.length <= 0 ){
+    function formErrorHanlder(e){
+    
+        let formValue = e.target.value;
+        let formName = e.target.name
 
-            setErrors(state =>({...state, name: 'Name cannot be blank'}))
+     
+        if(formValue.length <= 0){
+
+            setFormErrors(formErrors => ({...formErrors, [formName]: `${formName} cannot be blank`}))
 
         }
         else{
 
-            setErrors(state => ({...state,name: null}))
+            setFormErrors(formErrors => ({...formErrors, [formName]: null}))
 
         }
 
+
     }
+
 
     return(
 
@@ -85,6 +103,7 @@ export default function Edit(){
                     </div>
                 </div>
 
+    
             </div>
             <div className="container">
                 <div className="row">
@@ -93,49 +112,65 @@ export default function Edit(){
 
                             <form method="PUT" onSubmit={albumEditSubmitHandler}>
                             <div className="row">
+
                                     <h1>General Info:</h1>
                                     <h4>Album Name: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" style={{borderColor: errors.name ? 'red' : 'green'}} onBlur={nameChangeHandler} defaultValue={album.name} type="text" name="name" />
+                                        <input className="contactus" style={{borderColor: formErrors.name ? 'red' : 'green'}} onBlur={formErrorHanlder} defaultValue={album.name} placeholder="Name" type="text" name="name" />
                                     </div>
-                                    <span className={styles.error}>{errors.name}</span>
+                                    <span className={styles.error}>{formErrors.name}</span>
+
                                     <h4>Composer/Artist: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" placeholder="Name" type="text" defaultValue={album.composerArtist} name="composer-artist" />
+                                        <input className="contactus" style={{borderColor: formErrors.artist ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="Name" type="text" defaultValue={album.composerArtist} name="artist" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.artist}</span>
+
                                     <h4>Date of Release: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" placeholder="year" type="number" defaultValue={album.date} name="date" />
+                                        <input className="contactus" style={{borderColor: formErrors.date ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="year" type="number" defaultValue={album.date} name="date" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.date}</span>
+
                                     <h4>Number of Tracks: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" type="number" placeholder="№" defaultValue={album.numberOfSongs} name="number-of-songs" />
+                                        <input className="contactus" style={{borderColor: formErrors.tracks ? 'red' : 'green'}}  onBlur={formErrorHanlder} type="number" placeholder="№" defaultValue={album.numberOfSongs} name="tracks" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.tracks}</span>
+                                    
                                     <h4>Genre: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" placeholder="..." type="text" defaultValue={album.genre} name="genre" />
+                                        <input className="contactus"  style={{borderColor: formErrors.genre ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="..." type="text" defaultValue={album.genre} name="genre" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.genre}</span>
 
                                     <h1>Tracklist:</h1>
 
                                     <div className={styles.tracklistContainer}>
-                                        <textarea className={styles.tracklist} placeholder="..." type="text" defaultValue={album.tracklist} name="tracklist" />
+                                        <textarea className={styles.tracklist}  style={{borderColor: formErrors.tracklist ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="..." type="text" defaultValue={album.tracklist} name="tracklist" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.tracklist}</span>
                                     
                                     <h1>Additional Info:</h1>
 
                                     <h4>Description: </h4>
                                     <div className="col-sm-12">
-                                        <textarea className={styles.description} placeholder="..." type="text" defaultValue={album.description} name="description" />
+                                        <textarea className={styles.description} style={{borderColor: formErrors.description ? 'red' : 'green'}}  onBlur={formErrorHanlder} placeholder="..." type="text" defaultValue={album.description} name="description" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.description}</span>
+
                                     <h4>Personnel: </h4>
                                     <div className="col-sm-12">
-                                        <textarea className={styles.description} placeholder="..." type="text" defaultValue={album.personnel} name="personnel" />
+                                        <textarea className={styles.description} style={{borderColor: formErrors.personnel ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="..." type="text" defaultValue={album.personnel} name="personnel" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.personnel}</span>
+
                                     <h4>Album Art: </h4>
                                     <div className="col-sm-12">
-                                        <input className="contactus" placeholder="imageURL" type="text" defaultValue={album.imageURL} name="imageURL" />
+                                        <input className="contactus" style={{borderColor: formErrors.picture ? 'red' : 'green'}} onBlur={formErrorHanlder} placeholder="imageURL" type="text" defaultValue={album.imageURL} name="picture" />
                                     </div>
+                                    <span className={styles.error}>{formErrors.picture}</span>
+
                                     <div className="col-sm-12">
                                         <button className="send">Edit</button>
                                     </div>
