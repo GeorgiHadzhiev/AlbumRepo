3cd9cd756b676924d5de26443d788b3b015cc882
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-10 16:45:44 +0200

    Added error handling to the Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -38,12 +38,53 @@ export default function Login(){
 
     function emailErrorHandler(e){
 
+        let formEmailValue = e.target.value;
+        
+        let emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
 
+        if(formEmailValue.length <= 0){
+
+            setFormErrors(formErrors => ({...formErrors, email: `Email cannot be blank`}))
+
+        }
+        else if(formEmailValue.length > 40){
+
+            setFormErrors(formErrors => ({...formErrors, email: `Email cannot be more than 40 characters`}))
+
+        }
+        else if(!emailRegex.test(formEmailValue)){
+
+            setFormErrors(formErrors => ({...formErrors, email: `Email must be valid`}))
+
+        }
+        else{
+
+            setFormErrors(formErrors => ({...formErrors, email: null}))
+
+        }
 
     }
 
     function passwordErrorHandler(e){
 
+        let formPasswordValue = e.target.value;
+        
+
+        if(formPasswordValue.length <= 0){
+
+            setFormErrors(formErrors => ({...formErrors, password: `Password cannot be blank`}))
+
+        }
+        else if(formPasswordValue.length < 6 || formPasswordValue.length > 30){
+
+            setFormErrors(formErrors => ({...formErrors, password: `Password cannot be less than 6 characters or more than 30 characters`}))
+
+        }
+        else{
+
+            setFormErrors(formErrors => ({...formErrors, password: null}))
+
+        }
 
 
     }
@@ -67,10 +108,12 @@ export default function Login(){
                 <div ref={closeRef} typeof="button" data-bs-dismiss="offcanvas" aria-label="Close"></div>
             <div className="offcanvas-body" aria-label="Close">
                 <div>
+                        <span className="registrationError">{formErrors.email}</span>
                         <div className="form-floating mb-3">
                             <input onBlur={emailErrorHandler} type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name="email" />
                             <label htmlFor="floatingInput">Email address</label>
                         </div>
+                        <span className="registrationError">{formErrors.password}</span>
                         <div className="form-floating">
                             <input onBlur={passwordErrorHandler} type="password" className="form-control" id="floatingPassword" placeholder="Password"name="password" />
                             <label htmlFor="floatingPassword">Password</label>
