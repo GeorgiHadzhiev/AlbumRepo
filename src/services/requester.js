92e71f5cc23ff8b5a924ea336de56b588dea4bdc
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-12 03:14:10 +0200

    started work on a new requester. Makes the code easier to read

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,6 +1,32 @@
-export const request = (url) => {
+export const request = (method,url,data) => {
 
-    return fetch(url).then(responseHandler)
+    let result = null;
+
+    if(method === 'GET'){
+
+        result = fetch(url);
+
+    }
+    else{
+
+        let token = getToken();
+
+        result = fetch(url, {
+
+            method,
+            headers: {
+
+                'content-type': 'application/json',
+                'X-Authorization': token,
+
+            },
+            body: JSON.stringify(data),
+
+        });
+
+    }
+
+    return result.then(responseHandler)
 
 }
 
@@ -19,6 +45,30 @@ async function responseHandler(res){
 
     }
 
+}
 
+function getToken(){
+
+    try{
+
+        let user = localStorage.getItem('user');
+
+        if(!user){
+
+            throw {message: 'You must be loged in for this action'}
+
+        }
+
+        return user.accessToken;
+
+    }
+    catch(err){
+
+        console.log(err)
+
+    }
 
 }
+
+export const get = request.bind(null, 'GET');
+export const put = request.bind(null, 'PUT');
