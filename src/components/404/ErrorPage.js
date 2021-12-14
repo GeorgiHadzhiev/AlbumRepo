307f392cd5248eed778db41dd2c7063d6f53f650
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-14 23:16:54 +0200

    added stylings to the 404 page

--- src/components/404/ErrorPage.js
+++ src/components/404/ErrorPage.js
@@ -1,11 +1,22 @@
+import { Card } from "react-bootstrap"
 
+import styles from './ErrorPage.module.css'
 
 
 export default function ErrorPage(){
 
     return(
 
-        <h1>"Error"</h1>
+        <Card className="bg-white text-dark">
+            <Card.Img src="/images/404.jpg" alt="Card image" />
+            <Card.ImgOverlay>
+                <Card.Title className={styles.text}>OOPS</Card.Title>
+                <Card.Text className={styles.text}>
+                It seems you're in the wrong place
+                </Card.Text>
+                <Card.Text className={styles.text}>We can't find anything here...</Card.Text>
+            </Card.ImgOverlay>
+        </Card>
 
     )
 
