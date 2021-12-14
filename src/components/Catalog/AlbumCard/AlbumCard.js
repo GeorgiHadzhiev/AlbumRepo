c6b59e94d1af80ca604d99312b6352ad616eeb15
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-15 00:11:09 +0200

    Added sylings to the album cards on the Catalog component

--- src/components/Catalog/AlbumCard/AlbumCard.js
+++ src/components/Catalog/AlbumCard/AlbumCard.js
@@ -1,23 +1,23 @@
+import { Card,Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 export default function AlbumCard({album}){
 
     return(
-
-        <li>
-            <h3>Name: {album.name}</h3>
-            <p>Year: {album.date}</p>
-            <p>
-                <img src={album.picture} alt="broken"/>
-            </p>
-            <p>Likes: {album.likes.length} </p>
-            <p>Disslikes: {album.dislikes.length}</p>
-            <Link to={`/details/${album._id}`}>Details</Link>
-
-
-
-        </li>
-
-    )
+        
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={album.picture} />
+            <Card.Body>
+                <Card.Title>Name: {album.name}</Card.Title>
+                <Card.Title>Year: {album.date}</Card.Title>
+                <Card.Text>
+                <p>Likes: {album.likes.length} </p>
+                <p>Disslikes: {album.dislikes.length}</p>
+                </Card.Text>
+                <Button as={Link} to={`/details/${album._id}`} variant="primary">Details</Button>
+            </Card.Body>
+        </Card>
+        
+    );
 
 }
