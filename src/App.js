c9f1b5d5259ba96dad8a4dbdd84915d9cb3f1942
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2021-12-14 21:41:05 +0200

    Refacted the code so that the App contains all the routes now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
+import { Routes,Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import {AuthProvder} from './contexts/AuthContext.js'
-import Main from './components/Main/Main.js';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Register from './components/Register';
+import MyAlbums from './components/MyAlbums';
+import Catalog from './components/Catalog';
+import Add from './components/Add';
+import Details from './components/Details';
+import Edit from './components/Edit';
+import Logout from './components/Logout';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -11,8 +24,27 @@ function App() {
     <AuthProvder>
 
       <div className="App">
+
+        <Header />
+            
+        <AnimatePresence>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/MyAlbums" element={<MyAlbums />} />
+                <Route path="/add" element={<Add />} />
+                <Route path="/details/:albumId" element={<Details />} />
+                <Route path="/edit/:albumId" element={<Edit />} />
+                <Route path="/logout" element={<Logout />} />
+
+    
+            </Routes>
         
-        <Main/>
+        </AnimatePresence>
+
+        <Footer />
 
       </div>
 
