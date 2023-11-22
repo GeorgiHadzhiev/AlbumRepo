dfe65947d45a98087bd461a098aa632cc0d6fc44
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2023-11-22 16:23:08 +0200

    Changed the connection string in the backend from "localhost" to "127.0.0.1" since Mongoose wanted it that way to start up

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,4 +1,4 @@
 exports.PORT = 3030;
-exports.DB_CONNECTION_STRING = "mongodb://localhost:27017/AlbumRepo";
+exports.DB_CONNECTION_STRING = "mongodb://127.0.0.1:27017/AlbumRepo";
 exports.JWT_SECRET = 'secret';
 exports.AUTH_COOKIE_NAME = 'token'
