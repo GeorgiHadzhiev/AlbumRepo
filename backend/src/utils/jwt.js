576761448ab8d72fd8f299a3fa15a00e4dc1eae4
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-06-11 13:37:00 +0300

    corrected spelling mistake on a comment in the backend

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const util = require('util');
 
-// the hole point of this, is to make jwt have promises. 
+// the whole point of this, is to make jwt have promises. 
 // in this file, i initially import jwt, and then bellow i make it's certain functions become promisible
 
 exports.sign = util.promisify(jwt.sign);
